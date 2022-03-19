122aff3b3c802a0125a0ccb2d457774119e43751
codeax <codingax@foxmail.com>
2022-03-19 16:50:00 +0800

    feat: add upload

---
+++ api/common.js
@@ -0,0 +1,41 @@
+import instance from '../utils/api'
+const baseUrl = 'https://live-cdn.xgpax.top';
+const apiUrl = 'https://up-z2.qiniup.com/';
+
+export const getUploadToken = () => {
+    return instance.post('/upload');
+}
+
+export const file2qiniuCloud = async (file) => {
+    uni.showLoading({
+        title: '上传中'
+    })
+    try {
+        const res = await getUploadToken();
+        const { key, token } = res.data;
+        const formData = {
+            token,
+            key,
+        }
+        const response = await uni.uploadFile({
+            formData,
+            url: apiUrl,
+            name: 'file',
+            filePath: file.url
+        })
+        const result = JSON.parse(response[1].data);
+        if (!result.key) {
+            uni.showToast({
+                title: '上传失败'
+            })
+        }
+        return `${baseUrl}/${result.key}`;
+    } catch (e) {
+        uni.showToast({
+            title: '上传失败'
+        })
+    } finally {
+        uni.hideLoading()
+    }
+    return ''
+}
--- pages/editProfile/editProfile.vue
+++ pages/editProfile/editProfile.vue
@@ -7,10 +7,12 @@
     <u--form labelPosition="left" :model="formData" :rules="rules" ref="form">
       <u-form-item required label="头像" prop="avatarUrl" borderBottom>
         <u-upload
-          name="3"
+          name="1"
           :maxCount="1"
           :previewFullImage="true"
           :fileList="formData.avatarUrl"
+          @delete="deletePic"
+          @afterRead="afterRead"
         ></u-upload>
       </u-form-item>
       <u-form-item required label="昵称" prop="nickName" borderBottom>
@@ -42,7 +44,7 @@
 <script>
 import { defaultAvatarUrl } from "../../config";
 import { convert } from "../../utils/index";
-import { updateProfile } from "../../api";
+import { updateProfile, file2qiniuCloud } from "../../api";
 import { cloneDeep } from "lodash";
 
 export default {
@@ -52,7 +54,7 @@ export default {
       formData: {
         nickName: "",
         signature: "",
-        avatarUrl: "",
+        avatarUrl: [],
       },
       rules: {
         nickName: {
@@ -69,6 +71,7 @@ export default {
     };
   },
   mounted() {
+    // 初始化表单数据
     const currentUserInfo = convert(uni.getStorageSync("userInfo"));
     this.formData.nickName = currentUserInfo.nickName;
     this.formData.signature = currentUserInfo.signature;
@@ -79,17 +82,38 @@ export default {
     ];
   },
   methods: {
+    deletePic(event) {
+      this.formData.avatarUrl.splice(event.index, 1);
+    },
+    async afterRead(event) {
+      const { file } = event;
+      // 上传文件到七牛云
+      const targetUrl = await file2qiniuCloud(file);
+      // 更新头像
+      this.formData.avatarUrl = [
+        {
+          url: targetUrl,
+        },
+      ];
+    },
     async handleSubmit() {
+      // 提交表单
       await this.$refs.form.validate();
+      // 处理 userDetail
       const userDetail = cloneDeep(this.formData);
       userDetail.avatarUrl = userDetail.avatarUrl?.[0]?.url || defaultAvatarUrl;
       this.btnLoading = true;
       try {
         const res = await updateProfile(userDetail);
+        // 更新本地缓存
         this.$store.commit("setUserInfo", res.data);
+        uni.showToast({
+          title: "保存成功",
+        });
+        // 1s后跳转到个人中心
         setTimeout(function () {
           uni.navigateBack();
-        }, 500);
+        }, 1000);
       } finally {
         this.btnLoading = false;
       }
