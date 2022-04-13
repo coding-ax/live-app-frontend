ad735d6f39a0c4aba355070eded0ee38539fd72b
codeax <codingax@foxmail.com>
2022-04-13 16:10:20 +0800

    feat: add live

---
+++ components/describe.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="container">
+    <view class="container-item" v-for="item in data" :key="item.key">
+      <text class="key">{{ item.key }}</text>
+      <text class="sep"> : </text>
+      <text class="value">{{ item.value }}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #000;
+  opacity: 0.5;
+  padding: 4px;
+  border-radius: 4px;
+  .container-item {
+    @extend .flex-row;
+    .key {
+      width: 100px;
+      font-weight: 600;
+      color: #fff;
+    }
+    .sep {
+      color: #fff;
+    }
+
+    .value {
+      color: #fff;
+    }
+  }
+}
+</style>
